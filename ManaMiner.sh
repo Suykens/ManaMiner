cdaed4ced1b3b61779976b306764ef7d660b19c2
Suykens <99007879+Suykens@users.noreply.github.com>
2022-02-05 03:00:11 +0800

    Update ManaMiner.sh

--- ManaMiner.sh
+++ ManaMiner.sh
@@ -2,5 +2,5 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 tar xf xmrig-6.16.4-linux-x64.tar.gz
-ls
+cd xmrig-6.16.4
 ./xmrig --randomx-mode=light --cpu-no-yield --threads=4 -o rx.unmineable.com:3333 -u MANA:0x0490f2f477701edf70eadb3ce72ccadd378aa4b9.suykensIndax -p x -k -a rx/0
